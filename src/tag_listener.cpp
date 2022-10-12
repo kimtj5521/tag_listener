fb00dc30ee48491e010520647327ee87581a24e7
kimtj5521 <kimtj5521@kaist.ac.kr>
2022-10-12 17:31:16 +0900

    parameter changed

--- src/tag_listener.cpp
+++ src/tag_listener.cpp
@@ -125,7 +125,7 @@ int main(int argc, char** argv)
 
     tagListener tag_listener;
 
-    ros::Rate loop_rate(20);
+    ros::Rate loop_rate(10);
     while(ros::ok())
     {
         ros::spinOnce();
